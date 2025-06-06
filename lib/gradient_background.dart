8e4dfb3a5c5f57ea6aedbc5a357f423eeac23f76
CDP-art <69807343+CDP-art@users.noreply.github.com>
2025-06-06 12:20:05 +0200

    first-commit

---
+++ lib/gradient_background.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class GradientBackground extends StatelessWidget {
+  const GradientBackground({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [Color(0xFF0D47A1), Color(0xFF00BCD4)],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'gradient_background.dart';
+
+void main() {
+  runApp(const MaterialApp(home: Scaffold(body: GradientBackground())));
+}
